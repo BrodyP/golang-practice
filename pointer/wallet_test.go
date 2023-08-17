ccdb995a8bb3b6ece5384b070feddb5780d645c2
BrodyP <supanat.lahp@gmail.com>
2023-08-18 02:54:11 +0700

    pointer & errors

--- pointer/wallet_test.go
+++ pointer/wallet_test.go
@@ -1,21 +1,39 @@
 package pointer
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
-	got := wallet.Balance()
 
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+	assertionBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 
-	want := 10
-
-	if got != want {
-		t.Errorf("expect %d but got %d", want, got)
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("test deposit bitcoin", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertionBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("test withdraw bit coin", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertionBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("test insufficient fund", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		if err == nil {
+			t.Error("expect error")
+		}
+	})
 
 }
