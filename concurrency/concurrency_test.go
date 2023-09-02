5a16b68d99e1370b149f7304cf1c812bc8d87b15
BrodyP <supanat.lahp@gmail.com>
2023-09-02 13:53:20 +0700

    mocking && concurrency
    -mocking function for testing
    -go routine
    -go channel

---
+++ concurrency/concurrency_test.go
@@ -0,0 +1,54 @@
+package concurrency
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func mockWebsiteChecker(url string) bool {
+	return url != "waat://furhurterwe.geds"
+}
+
+func TestCheckWebsite(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("wanted %v, got %v", want, got)
+	}
+}
+
+/// benchmark test
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Microsecond)
+	return true
+}
+
+// BenchmarkCheckWebsite-4                1        1483366200 ns/op don't use go routine
+// BenchmarkCheckWebsite-4               76          15373120 ns/op use go routine
+func BenchmarkCheckWebsite(b *testing.B) {
+	urls := make([]string, 100)
+
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
